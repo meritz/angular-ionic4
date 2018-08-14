9d7ae5644807cf7ce0c53830b8043fb49d779d0c
koffi agbakou <modestemeritz@gmail.com>
2018-08-14 01:35:36 +0000

    conficuraion api & lafin de exo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://newsapi.org/v2',
+  apiKey: 'de8c024131b94277a57d19efbb7801b5',
 };
 
 /*
